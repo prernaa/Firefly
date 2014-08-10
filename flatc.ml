4b8bf7aa7c761afcbd0b7689061bf627583cc1cf
royaslan <roy.aslan@outlook.com>
2014-08-10 04:55:44 -0400

    Further extended flat c

--- flatc.ml
+++ flatc.ml
@@ -9,7 +9,7 @@ let c_statement (x, y, z) g ti li = match x with
 		Int(v) 	-> 	print_endline ("int t" ^ string_of_int !ti ^ " = " ^ string_of_int v ^ ";"); 
 					ti := !ti + 1; 
 					Stack.push (!ti - 1) stck
-	| 	Flt(v) 	-> 	print_endline ("int t" ^ string_of_int !ti ^ " = " ^ string_of_float v ^ ";"); 
+	| 	Flt(v) 	-> 	print_endline ("float t" ^ string_of_int !ti ^ " = " ^ string_of_float v ^ ";"); 
 					ti := !ti + 1;
 					Stack.push (!ti - 1) stck
 	|	Add_Op	-> 	let i1 = Stack.pop stck and i2 = Stack.pop stck in
@@ -21,8 +21,16 @@ let c_statement (x, y, z) g ti li = match x with
 					print_endline ("vec2 t" ^ string_of_int !ti ^ " = t" ^ string_of_int i2 ^ ", t" ^ string_of_int i1 ^ ";"); 
 					ti := !ti + 1;
 					Stack.push (!ti - 1) stck
-	|	Id(v)	-> 	()				
-	|	Asn_Op	->  ()
+	|	On_Op	-> 	let i1 = Stack.pop stck and i2 = Stack.pop stck in
+					(* need to get exact c++ syntax for openGL line and/or define a c++ function -- e.g., c_on *)
+					print_endline ("vec2 t" ^ string_of_int !ti ^ " = c_on(t" ^ string_of_int i2 ^ ", t" ^ string_of_int i1 ^ ")" ^ ";"); 
+					ti := !ti + 1;
+					Stack.push (!ti - 1) stck
+	|	Id(v)	-> 	(* need to distinguish between LHS and RHS. Here, I'm treating the RHS case *)
+					print_endline ("inferred_type t" ^ string_of_int !ti ^ " = glb_" ^ v ^ ";"); 
+					ti := !ti + 1;
+					Stack.push (!ti - 1) stck
+	|	Asn_Op	->  () (* we need the semantic tree to split up this operator into declaration and assignment *)
 	| 	_ 		->	()	
 
 
