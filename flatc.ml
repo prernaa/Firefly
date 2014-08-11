dbac0ec5115f38054ccdac3f512f874d29b2e610
#PRERNA CHIKERSAL# <PRERNA1@e.ntu.edu.sg>
2014-08-10 20:36:29 -0400

    added in flatc code gen for vec2cpp

--- flatc.ml
+++ flatc.ml
@@ -30,7 +30,11 @@ let c_statement (x, y, z) ti li = match x with
 	|	LessThanEq_Op	-> ()|	GreaterThan_Op	-> 	()
 	|	GreaterThanEq_Op	-> ()	
 	|	EqualsTo_Op	-> 	()
-	|	Vec2_Op	-> 	()
+	|	Vec2_Op	-> 	let op1 = Stack.pop stck and op2 = Stack.pop stck in
+					print_endline(z^" t"^ string_of_int(!temp_counter)^ " = {"^op2^" , "^op1^"};");
+					Stack.push ("t"^string_of_int(!temp_counter)) stck;
+					temp_counter:=!temp_counter+1;
+					()
 	|	On_Op	-> 	()
 	|   DAsn_Op ->  let op1 = Stack.pop stck and op2 = Stack.pop stck in
 					print_endline(z^" "^ op1 ^ " = "^op2^";");
