be94c9f3c94577142a57bdb07c39a6ed57b4d961
#PRERNA CHIKERSAL# <PRERNA1@e.ntu.edu.sg>
2014-08-10 22:50:57 -0400

    still some issues in printing cpp

--- flatc.ml
+++ flatc.ml
@@ -1,5 +1,10 @@
 open Semantics
 open Stack
+open Printf
+
+let file = "output.cpp"
+let oc = open_out file
+
 
 let stck = Stack.create ()
 let a = Stack.push ("hey") stck
@@ -11,21 +16,25 @@ let temp_counter = ref(0)
 (* We assume that a variable called _firefly is declared the initcpp code *)
 
 let c_statement (x, y, z) ti li = match x with		
-		Int(v) 	->  print_endline("int _t"^ string_of_int(!temp_counter)^ " = "^string_of_int(v)^";");
+		Int(v) 	->  (*seek_out oc flen;*)
+					(*let flen = out_channel_length oc in 
+					seek_out oc flen;
+					print_endline("");*)
+					fprintf oc "\n\t%s" ("int _t"^ string_of_int(!temp_counter)^ " = "^string_of_int(v)^";");
 					Stack.push ("_t"^string_of_int(!temp_counter)) stck;
 					temp_counter:=!temp_counter+1;
 					()	
-	| 	Flt(v) 	-> 	print_endline("float _t"^ string_of_int(!temp_counter)^ " = "^string_of_float(v)^";");
+	| 	Flt(v) 	-> 	fprintf oc "\n\t%s" ("float _t"^ string_of_int(!temp_counter)^ " = "^string_of_float(v)^";");
 					Stack.push ("_t"^string_of_int(!temp_counter)) stck;
 					temp_counter:=!temp_counter+1;
 					()
 	|	Add_Op	-> 	let op1 = Stack.pop stck and op2 = Stack.pop stck in
-					print_endline(z^" _t"^ string_of_int(!temp_counter)^ " = "^op2^" + "^op1^";");
+					fprintf oc "\n\t%s" (z^" _t"^ string_of_int(!temp_counter)^ " = "^op2^" + "^op1^";");
 					Stack.push ("_t"^string_of_int(!temp_counter)) stck;
 					temp_counter:=!temp_counter+1;
 					()
 	|	Minus_Op -> let op1 = Stack.pop stck and op2 = Stack.pop stck in
-					print_endline(z^" _t"^ string_of_int(!temp_counter)^ " = "^op2^" - "^op1^";");
+					fprintf oc "\n\t%s" (z^" _t"^ string_of_int(!temp_counter)^ " = "^op2^" - "^op1^";");
 					Stack.push ("_t"^string_of_int(!temp_counter)) stck;
 					temp_counter:=!temp_counter+1;
 					()	
@@ -34,13 +43,13 @@ let c_statement (x, y, z) ti li = match x with
 	|	GreaterThanEq_Op	-> ()	
 	|	EqualsTo_Op	-> 	()
 	|	Vec2_Op	-> 	let op1 = Stack.pop stck and op2 = Stack.pop stck in
-					print_endline(z^" _t"^ string_of_int(!temp_counter)^ " = {"^op2^" , "^op1^"};");
+					fprintf oc "\n\t%s" (z^" _t"^ string_of_int(!temp_counter)^ " = {"^op2^" , "^op1^"};");
 					Stack.push ("_t"^string_of_int(!temp_counter)) stck;
 					temp_counter:=!temp_counter+1;
 					()
 	|	On_Op	-> 	(*STILL INCOMPLETE*)
 					let dist = Stack.pop stck and dir = Stack.pop stck in 
-					print_endline("float _t"^string_of_int(!temp_counter)^" = sqrt(("^dir^".x * "^dir^".x + "^dir^".y * "^dir^".y));
+					fprintf oc "\n\t%s" ("float _t"^string_of_int(!temp_counter)^" = sqrt(("^dir^".x * "^dir^".x + "^dir^".y * "^dir^".y));
 "^dir^".x = "^dir^".x/_t"^string_of_int(!temp_counter)^";
 "^dir^".y = "^dir^".y/_t"^string_of_int(!temp_counter)^";
 vec2cpp _t"^string_of_int(!temp_counter+1)^" = {"^dist^" * "^dir^".x + _ff.x , "^dist^" * "^dir^".y + _ff.y };
@@ -57,11 +66,11 @@ glEnd();
 					()
 	|   Off_Op	->  ()
 	|   DAsn_Op ->  let op1 = Stack.pop stck and op2 = Stack.pop stck in
-					print_endline(z^" "^ op1 ^ " = "^op2^";");
+					fprintf oc "\n\t%s" (z^" "^ op1 ^ " = "^op2^";");
 					Stack.push (op1) stck;
 					()
 	|	Asn_Op	->  let op1 = Stack.pop stck and op2 = Stack.pop stck in
-					print_endline(op1 ^ " = "^op2^";");
+					fprintf oc "\n\t%s" (op1 ^ " = "^op2^";");
 					Stack.push (op1) stck;
 					()
 	|	Id(v)	-> 	let lenSubs = ((String.length y) -4) in 
