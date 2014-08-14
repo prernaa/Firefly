b50bef682ba58a99ef3d2421433991842bedbcb5
#PRERNA CHIKERSAL# <PRERNA1@e.ntu.edu.sg>
2014-08-13 22:36:42 -0400

    test goto *ptr cpp

---
+++ testcpp.cpp
@@ -0,0 +1,123 @@
+
+
+
+#include <iostream>
+#include <GL/glut.h>  // GLUT, includes glu.h and gl.h
+#include <math.h>
+#ifdef _MSC_VER                         // Check if MS Visual C compiler
+#  pragma comment(lib, "opengl32.lib")  // Compiler-specific directive to avoid manually configuration
+#  pragma comment(lib, "glu32.lib")     // Link libraries
+#endif			
+using namespace std;
+struct vec2cpp{
+	float x;
+	float y;
+};
+vec2cpp _ff = {0,0};
+int myprogram();
+void DrawAxes()
+{
+	glBegin(GL_LINES);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex2f(-1.0f, 0.0f);
+		glVertex2f(+1.0f, 0.0f);
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex2f(0.0f, -1.0f);
+		glVertex2f(0.0f, +1.0f);
+	glEnd();
+}
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+   	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+
+	// Render a color-cube consisting of 6 quads with different colors
+   	glLoadIdentity();                 // Reset the model-view matrix
+
+   	DrawAxes();
+	myprogram();
+
+  	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+}
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(0.0, 0.0, 0.0);
+   	glClearDepth(1.0f);                   // Set background depth to farthest
+   	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+   	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+   	glShadeModel(GL_SMOOTH);   // Enable smooth shading
+   	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+}
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+// Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+ 
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+ 
+   // Set the aspect ratio of the clipping area to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();
+   if (width >= height) {
+     // aspect >= 1, set the height from -1 to 1, with larger width
+      gluOrtho2D(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0);
+   } else {
+      // aspect < 1, set the width to -1 to 1, with larger height
+     gluOrtho2D(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect);
+   }
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);          // Initialize GLUT
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);	// Enable double buffering
+   	glutInitWindowSize(640, 480);   // Set the window's initial width & height - non-square
+   	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   	glutCreateWindow("Model Transform");  // Create window with the given title
+   	glutDisplayFunc(display);       // Register callback handler for window re-paint event
+   	glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+   	init();                       // Our own OpenGL initialization
+   	glutMainLoop();                 // Enter the infinite event-processing loop
+	return 0;
+}
+int myprogram(){
+
+	void* the_label_pointer; // mycode
+    
+	int _t0 = 0;
+	int i = _t0;
+	float _t1 = 0.3;
+	float j = _t1;
+	_L11:
+    the_label_pointer = &&_L11; // mycode
+	int _t2 = 20;
+	bool _t3 = _t2 == i;
+	bool _t4 = !_t3;
+	if (!_t4) { goto _L10; }{
+	int _t5 = 1;
+	int _t6 = 2;
+	int _t7 = _t5 * _t6;
+	int _t8 = 2;
+	int _t9 = _t7 / _t8;
+	int _t10 = i + _t9;
+	i = _t10;
+	int _t11 = 10;
+	bool _t12 = i > _t11;
+	if (_t12) { goto _L20; } {{
+	float _t13 = 0.2;
+	float _t14 = j + _t13;
+	j = _t14;
+	goto _L21;
+	}_L20:{
+	float _t15 = 0.2;
+	float _t16 = j - _t15;
+	j = _t16;
+	}}
+	_L21:
+	goto *the_label_pointer; //changed to mycode
+	}_L10:{
+	}
+
+
+	return 0;
+}
+
